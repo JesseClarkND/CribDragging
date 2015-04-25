2e4e6d9910f3b4df71ba1e424b5270076183efbf
Jesse Clark <hogarth45@gmail.com>
2015-04-25 12:18:55 -0500

    sln added

---
+++ CribDragging/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+namespace CribDragging
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && this.components != null)
+            {
+                this.components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.HexBox1 = new RichTextBox();
+            this.HexBox2 = new RichTextBox();
+            this.label1 = new Label();
+            this.label2 = new Label();
+            this.CribBox1 = new ListBox();
+            this.CribFindings = new ListBox();
+            this.AddCribButt = new Button();
+            this.AddCrib = new TextBox();
+            this.DelButt = new Button();
+            this.KeyBox = new RichTextBox();
+            this.label3 = new Label();
+            this.button1 = new Button();
+            this.label4 = new Label();
+            this.XORBox = new TextBox();
+            this.CribberBox = new TextBox();
+            this.CribYieldings = new TextBox();
+            this.label5 = new Label();
+            this.label6 = new Label();
+            this.label7 = new Label();
+            this.label8 = new Label();
+            base.SuspendLayout();
+            this.HexBox1.Location = new Point(33, 38);
+            this.HexBox1.Name = "HexBox1";
+            this.HexBox1.Size = new Size(549, 129);
+            this.HexBox1.TabIndex = 0;
+            this.HexBox1.Text = "";
+            this.HexBox2.Location = new Point(33, 195);
+            this.HexBox2.Name = "HexBox2";
+            this.HexBox2.Size = new Size(549, 129);
+            this.HexBox2.TabIndex = 1;
+            this.HexBox2.Text = "";
+            this.label1.AutoSize = true;
+            this.label1.Location = new Point(33, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new Size(128, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Enter CipherText1 in Hex:";
+            this.label2.AutoSize = true;
+            this.label2.Location = new Point(33, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new Size(128, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Enter CipherText2 in Hex:";
+            this.CribBox1.FormattingEnabled = true;
+            this.CribBox1.Location = new Point(602, 54);
+            this.CribBox1.Name = "CribBox1";
+            this.CribBox1.Size = new Size(120, 381);
+            this.CribBox1.TabIndex = 5;
+            this.CribBox1.SelectedIndexChanged += new EventHandler(this.CribBox1_SelectedIndexChanged);
+            this.CribFindings.FormattingEnabled = true;
+            this.CribFindings.Location = new Point(746, 54);
+            this.CribFindings.Name = "CribFindings";
+            this.CribFindings.Size = new Size(741, 563);
+            this.CribFindings.TabIndex = 6;
+            this.CribFindings.SelectedIndexChanged += new EventHandler(this.CribFindings_SelectedIndexChanged);
+            this.AddCribButt.Location = new Point(454, 361);
+            this.AddCribButt.Name = "AddCribButt";
+            this.AddCribButt.Size = new Size(128, 23);
+            this.AddCribButt.TabIndex = 7;
+            this.AddCribButt.Text = "Add to Crib List->";
+            this.AddCribButt.UseVisualStyleBackColor = true;
+            this.AddCribButt.Click += new EventHandler(this.AddCribButt_Click);
+            this.AddCrib.Location = new Point(454, 335);
+            this.AddCrib.Name = "AddCrib";
+            this.AddCrib.Size = new Size(128, 20);
+            this.AddCrib.TabIndex = 8;
+            this.DelButt.Location = new Point(602, 441);
+            this.DelButt.Name = "DelButt";
+            this.DelButt.Size = new Size(120, 24);
+            this.DelButt.TabIndex = 9;
+            this.DelButt.Text = "Delete word";
+            this.DelButt.UseVisualStyleBackColor = true;
+            this.DelButt.Click += new EventHandler(this.DelButt_Click);
+            this.KeyBox.Location = new Point(33, 504);
+            this.KeyBox.Name = "KeyBox";
+            this.KeyBox.Size = new Size(549, 108);
+            this.KeyBox.TabIndex = 10;
+            this.KeyBox.Text = "";
+            this.label3.AutoSize = true;
+            this.label3.Location = new Point(30, 488);
+            this.label3.Name = "label3";
+            this.label3.Size = new Size(38, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Notes:";
+            this.button1.Location = new Point(33, 330);
+            this.button1.Name = "button1";
+            this.button1.Size = new Size(75, 29);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Begin It";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new EventHandler(this.button1_Click);
+            this.label4.AutoSize = true;
+            this.label4.Location = new Point(30, 401);
+            this.label4.Name = "label4";
+            this.label4.Size = new Size(153, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "CipherText1 XOR CipherText2:";
+            this.XORBox.Location = new Point(188, 398);
+            this.XORBox.Name = "XORBox";
+            this.XORBox.Size = new Size(394, 20);
+            this.XORBox.TabIndex = 16;
+            this.CribberBox.Location = new Point(188, 424);
+            this.CribberBox.Name = "CribberBox";
+            this.CribberBox.Size = new Size(394, 20);
+            this.CribberBox.TabIndex = 17;
+            this.CribYieldings.Location = new Point(188, 463);
+            this.CribYieldings.Name = "CribYieldings";
+            this.CribYieldings.Size = new Size(394, 20);
+            this.CribYieldings.TabIndex = 18;
+            this.label5.AutoSize = true;
+            this.label5.Location = new Point(599, 38);
+            this.label5.Name = "label5";
+            this.label5.Size = new Size(28, 13);
+            this.label5.TabIndex = 19;
+            this.label5.Text = "Crib:";
+            this.label6.AutoSize = true;
+            this.label6.Location = new Point(743, 38);
+            this.label6.Name = "label6";
+            this.label6.Size = new Size(54, 13);
+            this.label6.TabIndex = 20;
+            this.label6.Text = "Crib Drag:";
+            this.label7.AutoSize = true;
+            this.label7.Location = new Point(121, 427);
+            this.label7.Name = "label7";
+            this.label7.Size = new Size(61, 13);
+            this.label7.TabIndex = 21;
+            this.label7.Text = "Crib in Hex:";
+            this.label8.AutoSize = true;
+            this.label8.Location = new Point(30, 447);
+            this.label8.Name = "label8";
+            this.label8.Size = new Size(200, 13);
+            this.label8.TabIndex = 22;
+            this.label8.Text = "CipherText1 XOR CipherText2 XOR Crib:";
+            base.AutoScaleDimensions = new SizeF(6f, 13f);
+            base.AutoScaleMode = AutoScaleMode.Font;
+            base.ClientSize = new Size(1499, 624);
+            base.Controls.Add(this.label8);
+            base.Controls.Add(this.label7);
+            base.Controls.Add(this.label6);
+            base.Controls.Add(this.label5);
+            base.Controls.Add(this.CribYieldings);
+            base.Controls.Add(this.CribberBox);
+            base.Controls.Add(this.XORBox);
+            base.Controls.Add(this.label4);
+            base.Controls.Add(this.button1);
+            base.Controls.Add(this.label3);
+            base.Controls.Add(this.KeyBox);
+            base.Controls.Add(this.DelButt);
+            base.Controls.Add(this.AddCrib);
+            base.Controls.Add(this.AddCribButt);
+            base.Controls.Add(this.CribFindings);
+            base.Controls.Add(this.CribBox1);
+            base.Controls.Add(this.label2);
+            base.Controls.Add(this.label1);
+            base.Controls.Add(this.HexBox2);
+            base.Controls.Add(this.HexBox1);
+            base.Name = "Form1";
+            this.Text = "Form1";
+            base.Load += new EventHandler(this.Form1_Load);
+            base.ResumeLayout(false);
+            base.PerformLayout();
+        }
+
+        #endregion
+    }
+}
+
---
+++ CribDragging/Form1.cs
@@ -0,0 +1,249 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CribDragging
+{
+    public partial class Form1 : Form
+    {
+        private string[,] StringResults;
+        private byte[, ,] Results;
+        private byte[,] Kerbed;
+        private int bucket = 0;
+        //private IContainer components = null;
+        private RichTextBox HexBox1;
+        private RichTextBox HexBox2;
+        private Label label1;
+        private Label label2;
+        private ListBox CribBox1;
+        private ListBox CribFindings;
+        private Button AddCribButt;
+        private TextBox AddCrib;
+        private Button DelButt;
+        private RichTextBox KeyBox;
+        private Label label3;
+        private Button button1;
+        private Label label4;
+        private TextBox XORBox;
+        private TextBox CribberBox;
+        private TextBox CribYieldings;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        public Form1()
+        {
+            this.InitializeComponent();
+        }
+        private void AddCribButt_Click(object sender, EventArgs e)
+        {
+            if (this.AddCrib.Text != "")
+            {
+                this.CribBox1.Items.Add(this.AddCrib.Text);
+                this.CribBox1.Enabled = false;
+                this.AddCrib.Text = "";
+            }
+        }
+        private void DelButt_Click(object sender, EventArgs e)
+        {
+            this.CribBox1.Items.RemoveAt(this.CribBox1.SelectedIndex);
+            this.CribBox1.SelectedIndex = 0;
+        }
+        private int HexToInt(char hexChar)
+        {
+            hexChar = char.ToUpper(hexChar);
+            return (int)((hexChar < 'A') ? (hexChar - '0') : ('\n' + (hexChar - 'A')));
+        }
+        private int KerbedLength(int i)
+        {
+            int j;
+            for (j = 0; j < 10; j++)
+            {
+                if (this.Kerbed[i, j] == 0)
+                {
+                    break;
+                }
+            }
+            return j;
+        }
+        private void start_the_madness(int x, int y, byte[] RunAlong)
+        {
+            this.Results = new byte[x, y, 100];
+            for (int i = 0; i < x; i++)
+            {
+                int num = this.KerbedLength(i);
+                for (int j = 0; j < y - num + 1; j++)
+                {
+                    int num2 = 0;
+                    while (this.Kerbed[i, num2] != 0)
+                    {
+                        if (j + num2 < RunAlong.Length)
+                        {
+                            this.Results[i, j, num2] = (byte)(this.Kerbed[i, num2] ^ RunAlong[j + num2]);
+                        }
+                        num2++;
+                    }
+                }
+            }
+        }
+        private void string_the_madness(int x, int y)
+        {
+            this.StringResults = new string[x, y];
+            for (int i = 0; i < x; i++)
+            {
+                for (int j = 0; j < y; j++)
+                {
+                    string text = string.Format("{0}{1}", Convert.ToString(j), ": ");
+                    int num = 0;
+                    while (this.Results[i, j, num] != 0)
+                    {
+                        text = string.Format("{0}{1}", text, Convert.ToChar(this.Results[i, j, num]));
+                        num++;
+                    }
+                    this.StringResults[i, j] = text;
+                }
+            }
+        }
+        private void print_the_madness()
+        {
+            this.CribBox1.SetSelected(0, true);
+        }
+        private void kerberize()
+        {
+            this.Kerbed = new byte[this.CribBox1.Items.Count, 100];
+            for (int i = 0; i < this.CribBox1.Items.Count; i++)
+            {
+                for (int j = 0; j < this.CribBox1.Items[i].ToString().Length; j++)
+                {
+                    this.Kerbed[i, j] = Convert.ToByte(this.CribBox1.Items[i].ToString()[j]);
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (this.HexBox1.Text != "" && this.HexBox2.Text != "")
+            {
+                this.HexBox1.Text = this.HexBox1.Text.Replace(" ", string.Empty);
+                this.HexBox2.Text = this.HexBox2.Text.Replace(" ", string.Empty);
+                this.HexBox1.Text = this.HexBox1.Text.Replace("  ", string.Empty);
+                this.HexBox2.Text = this.HexBox2.Text.Replace("  ", string.Empty);
+                char[] array = this.HexBox1.Text.ToCharArray();
+                char[] array2 = this.HexBox2.Text.ToCharArray();
+                byte[] array3 = new byte[array.Length];
+                byte[] array4 = new byte[array2.Length];
+                byte[] array5 = new byte[(array.Length <= array2.Length) ? (array.Length / 2) : (array2.Length / 2)];
+                int num = 0;
+                int num2 = 0;
+                while (num2 + 1 < array.Length && num2 + 1 < array2.Length)
+                {
+                    array3[num] = Convert.ToByte(int.Parse(string.Format("{0}{1}", array[num2], array[num2 + 1]), NumberStyles.HexNumber));
+                    array4[num] = Convert.ToByte(int.Parse(string.Format("{0}{1}", array2[num2], array2[num2 + 1]), NumberStyles.HexNumber));
+                    array5[num] = (byte)(array3[num] ^ array4[num]);
+                    num++;
+                    num2 += 2;
+                }
+                this.CribBox1.Enabled = true;
+                this.bucket = array5.Length;
+                string text = string.Empty;
+                for (int i = 0; i < this.bucket; i++)
+                {
+                    text = string.Format("{0}{1}{2}", text, array5[i].ToString("X2"), " ");
+                }
+                text = text.TrimEnd(new char[0]);
+                this.XORBox.Text = text;
+                this.kerberize();
+                this.start_the_madness(this.CribBox1.Items.Count, array5.Length, array5);
+                this.string_the_madness(this.CribBox1.Items.Count, array5.Length);
+                this.print_the_madness();
+            }
+            else
+            {
+                this.HexBox1.Text = "3b101c091d53320c000910";
+                this.HexBox2.Text = "071d154502010a04000419";
+            }
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            this.CribBox1.HorizontalScrollbar = true;
+            this.CribFindings.HorizontalScrollbar = true;
+            this.CribBox1.Items.Add(" the ");
+            this.CribBox1.Items.Add(" be ");
+            this.CribBox1.Items.Add(" to ");
+            this.CribBox1.Items.Add(" of ");
+            this.CribBox1.Items.Add(" and ");
+            this.CribBox1.Items.Add(" a ");
+            this.CribBox1.Items.Add(" in ");
+            this.CribBox1.Items.Add(" that ");
+            this.CribBox1.Items.Add(" have ");
+            this.CribBox1.Items.Add(" I ");
+            this.CribBox1.Items.Add(" it ");
+            this.CribBox1.Items.Add(" for ");
+            this.CribBox1.Items.Add(" not ");
+        }
+        private void CribBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            this.CribFindings.Items.Clear();
+            if (this.CribBox1.SelectedIndex != -1)
+            {
+                string[] array = new string[this.bucket];
+                for (int i = 0; i < this.bucket; i++)
+                {
+                    array[i] = this.StringResults[this.CribBox1.SelectedIndex, i];
+                }
+                this.CribFindings.Items.AddRange(array);
+            }
+        }
+        private void CribFindings_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int length = this.CribBox1.Items[this.CribBox1.SelectedIndex].ToString().Length;
+            string text = string.Empty;
+            for (int i = 0; i < this.bucket; i++)
+            {
+                if (i == this.CribFindings.SelectedIndex)
+                {
+                    for (int j = 0; j < length; j++)
+                    {
+                        text = string.Format("{0}{1}{2}", text, this.Kerbed[this.CribBox1.SelectedIndex, j].ToString("X2"), " ");
+                        i++;
+                    }
+                    i--;
+                }
+                else
+                {
+                    text = string.Format("{0}{1}{2}", text, "00", " ");
+                }
+            }
+            text = text.Trim();
+            this.CribberBox.Text = text;
+            string text2 = string.Empty;
+            for (int k = 0; k < this.bucket; k++)
+            {
+                if (k == this.CribFindings.SelectedIndex)
+                {
+                    for (int j = 0; j < length; j++)
+                    {
+                        text2 = string.Format("{0}{1}{2}", text2, this.Results[this.CribBox1.SelectedIndex, this.CribFindings.SelectedIndex, j].ToString("X2"), " ");
+                        k++;
+                    }
+                    k--;
+                }
+                else
+                {
+                    text2 = string.Format("{0}{1}{2}", text2, "00", " ");
+                }
+            }
+            text2 = text2.TrimEnd(new char[0]);
+            this.CribYieldings.Text = text2;
+        }
+
+
+
+    }
+}
